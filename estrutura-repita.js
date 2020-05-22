bdd95d8d47db5aefb8004752e634110547e661e9
RodrigoMurulo <65743315+RodrigoMurulo@users.noreply.github.com>
2020-05-21 21:37:40 -0300

    Add files via upload

---
+++ estrutura-repita.js
@@ -0,0 +1,20 @@
+/*
+  sairLoop: caractere
+   valor01, valor02: real
+
+Inicio
+// Seção de Comandos, procedimento, funções, operadores, etc... 
+   repita
+      
+   */
+
+  function acaoBotao() {
+    var sairLoop, valor01, valor02;
+    do{
+        valor01 = prompt("Digite o primeiro valor: ")
+        valor02 = prompt("Digite o segundo valor: ")
+        document.getElementById("paragrafo").innerText = "Resultado: " + (parseInt(valor01) + parseInt(valor02))
+        sairLoop = prompt("Deseja sair? S/N")
+    }while(sairLoop == "N")
+
+    }
